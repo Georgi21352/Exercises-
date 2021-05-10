210a45833607ffbe029e4ed45b64f2a97e67ef99
Georgi21352 <83609188+Georgi21352@users.noreply.github.com>
2021-05-10 13:50:23 +0300

    Delete Zadanie_5.py

--- Zadanie_5.py
+++
@@ -1,9 +0,0 @@
-a=int(input("a="))
-b=int(input("b="))
-c=int(input("c="))
-if b<=a>=c:
-    print("large number", a)
-elif a<=b>=c:
-    print("large number", b)
-else:
-    print("large number", c)
